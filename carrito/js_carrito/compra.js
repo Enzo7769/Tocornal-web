40a3670e060583adc026a805abb00955999f5f3d
Enzo7769 <enzo.torres04@inacapmail.cl>
2024-09-26 22:45:13 -0300

    Tocornal web hecho en 2019 en inacap debo arreglar todo para cv

---
+++ carrito/js_carrito/compra.js
@@ -0,0 +1,74 @@
+const compra = new Carrito();
+const listaCompra = document.querySelector("#lista-compra tbody");
+const carrito = document.getElementById('carrito');
+const procesarCompraBtn = document.getElementById('procesar-compra');
+const cliente = document.getElementById('cliente');
+const correo = document.getElementById('correo');
+
+
+cargarEventos();
+
+function cargarEventos() {
+    document.addEventListener('DOMContentLoaded', compra.leerLocalStorageCompra());
+
+    //Eliminar productos del carrito
+    carrito.addEventListener('click', (e) => { compra.eliminarProducto(e) });
+
+    compra.calcularTotal();
+
+    //cuando se selecciona procesar Compra
+    procesarCompraBtn.addEventListener('click', procesarCompra);
+
+    carrito.addEventListener('change', (e) => { compra.obtenerEvento(e) });
+    carrito.addEventListener('keyup', (e) => { compra.obtenerEvento(e) });
+
+
+}
+
+function procesarCompra() {
+    // e.preventDefault();
+    if (compra.obtenerProductosLocalStorage().length === 0) {
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'No hay productos, selecciona alguno',
+            showConfirmButton: false,
+            timer: 2000
+        }).then(function () {
+            window.location = "../index.html";   //ver porque no regresa al inicio
+        })
+    }
+    else if (cliente.value === '' || correo.value === '') {
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Ingrese el Cliente y el Correo',
+            showConfirmButton: false,
+            timer: 4000
+        })
+    }
+    else {
+        
+            const cargandoGif = document.querySelector('#cargando');
+            cargandoGif.style.display = 'block';
+
+            const enviado = document.createElement('img');
+            enviado.src = 'img/mail.gif';
+            enviado.style.display = 'block';
+            enviado.width = '150';
+
+            
+            setTimeout(()=>{
+             cargandoGif.style.display = 'none';
+            document.querySelector('#loaders').appendChild(enviado);
+             }, 3000);  
+            
+            setTimeout(() => {
+            compra.vaciarLocalStorage();
+            enviado.remove();
+            window.location = "..index.html";
+            }, 2000);
+
+    }
+}
+
